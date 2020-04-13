44ca3793d6c34769543ccddff2bc5e2bc405cd14
jumpwmk <methus159357@gmail.com>
2020-04-13 12:04:19 +0700

    add https

--- app/server.js
+++ app/server.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
+// const express = require('express');
+// const https = require('https');
+// const fs = require('fs');
 
-var key = fs.readFileSync('../../selfsigned.key');
-var cert = fs.readFileSync('../../selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert,
-};
+// var key = fs.readFileSync('../../selfsigned.key');
+// var cert = fs.readFileSync('../../selfsigned.crt');
+// var options = {
+//   key: key,
+//   cert: cert,
+// };
 
 const app = require('./app');
 const port = process.env.PORT || 3001;
 
-var server = https.createServer(options, app);
+// var server_tmp = https.createServer(options, app);
 
 const server = app.listen(port, () => {
   console.log('Express server listening on port ' + port);
