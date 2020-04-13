0e60525065f448cd449c5e43484651327193b6a0
jumpwmk <methus159357@gmail.com>
2020-04-13 17:38:18 +0700

    add node

--- app/server.js
+++ app/server.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
-const https = require('https');
-const fs = require('fs');
+// const https = require('https');
+// const fs = require('fs');
 
-var key = fs.readFileSync('/home/ubuntu/selfsigned.key');
-var cert = fs.readFileSync('/home/ubuntu/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert,
-};
+// var key = fs.readFileSync('/home/ubuntu/selfsigned.key');
+// var cert = fs.readFileSync('/home/ubuntu/selfsigned.crt');
+// var options = {
+//   key: key,
+//   cert: cert,
+// };
 
 const app = require('./app');
 const port = process.env.PORT || 3001;
 
-var server_tmp = https.createServer(options, app);
+// var server_tmp = https.createServer(options, app);
 
-const server = server_tmp.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Express server listening on port ' + port);
 });
